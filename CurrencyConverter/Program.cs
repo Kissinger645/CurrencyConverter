927e039de6a4438a758c234f63d1ea27fac9fdc4
Bobby <kissinger645@gmail.com>
2017-01-19 09:10:44 -0500

    got rate from dictionary

--- CurrencyConverter/Program.cs
+++ CurrencyConverter/Program.cs
@@ -10,20 +10,18 @@ namespace CurrencyConverter
     {
         static void Main(string[] args)
         {
-        double rate;
-        string symbol;
         double currencyFromRate = 0;
         double currencyToRate = 0;
 
-            Dictionary<string, double> currency = new Dictionary<string, double>();
-            currency.Add("USD", 1);
-            currency.Add("BTC", .00115368);
-            currency.Add("EUR", .937635);
-            currency.Add("JPY", 113.622);
+        Dictionary<string, double> currency = new Dictionary<string, double>();
+        currency.Add("USD", 1);
+        currency.Add("BTC", .00115368);
+        currency.Add("EUR", .937635);
+        currency.Add("JPY", 113.622);
 
             Console.WriteLine("Enter Amount That You Would Like Converted");
             float amount = float.Parse(Console.ReadLine());
-            Console.WriteLine("Choose currency you are converting from: USD, JPY, EUR or BTC");//change once finished
+            Console.WriteLine("Choose currency you are converting from: USD, JPY, EUR or BTC");
             var currencyFrom = Console.ReadLine();
 
             // if currencyFrom is in the currency list set currencyFromRate = rate
@@ -42,8 +40,10 @@ namespace CurrencyConverter
             {
                 currencyToRate = currency["currencyTo"];
             }
-            var newAmount = amount / currencyFromRate * currencyToRate;
+            double newAmount = amount / currencyFromRate * currencyToRate;
             Console.WriteLine($"{amount} {currencyFrom} = {newAmount} {currencyTo}");
+
+            Console.ReadKey();
 
 
 
